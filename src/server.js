ae2bb6d198e6307edb90aa51f0837c0c1c8b79be
bayronPalacio <58010308+bayronPalacio@users.noreply.github.com>
2021-02-14 22:04:02 -0800

    Small change

--- src/server.js
+++ src/server.js
@@ -2,6 +2,6 @@ import express from 'express';
 
 const app = express();
 
-app.get('/hello', (req, res) => res.send('hello'));
+app.get('/helloFromByron', (req, res) => res.send('hello From Byron'));
 
 app.listen(8000, () => console.log('Listening on port 8000'));
